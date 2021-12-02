2dbef95960e3f57df320bbe8b7c40a8f9eca2940
OliviafortuneUCD <Test@test.com>
2021-12-02 02:06:23 +0000

    New

--- Alittleonindex.py
+++ Alittleonindex.py
@@ -2,38 +2,43 @@ import pandas as pd
 import numpy as np
 import matplotlib as mpl
 from matplotlib import pyplot as plt
+import seaborn as sns
 
 # making data frame from csv file
 # we build an index to make a quicker search
 
 df = pd.read_csv("Stores.csv", index_col="Customer_Number")
-
-print(df.head)
+#print the header
+#print(df.head)
 
 #reterieve by row
-print(df.iloc[[0]])
+#print(df.iloc[[0]])
 
 #reterieve by row 2
-print(df.iloc[[2]])
+#print(df.iloc[[1]])
 
 #reterive by 0 to 1
-print(df.iloc[[0, 1]])
+#print(df.iloc[[0, 1]])
 
 
-print(df.iloc[-1]) # last row of data frame
+#print(df.iloc[-1]) # last row of data frame
 
-print(df.iloc[:,0]) # first column of data frame (first_name)
+#print(df.iloc[:,0]) # first column of data frame (first_name)
 
-print(df.iloc[0:5]) # first five rows of dataframe
+#print(df.iloc[0:5]) # first five rows of dataframe
 
-#Why do we set the index. To make it quicker
+#Why do we set the index. To make it quicker and searchable
 df.set_index("Customer_name", inplace=True)
 
 #search for tescos
-print(df.loc['Tescos'])
+#print(df.loc['Tescos'])
 
 #search for tescos
-print("I was searching for Tescos....." , df.loc['Tescos'])
+#print("I was searching for Tescos....." , df.loc['Tescos'])
 
 
-print(df.loc[df['email'].str.endswith("@aldi.com")])
+#print(df.loc[df['email'].str.endswith("@aldi.com")])
+x=df['cost']
+y=df['price']
+#sns.lineplot(data=df, x=x, y=y)
+
---
+++ tips2.py
@@ -0,0 +1,15 @@
+import seaborn as sns
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# loading of a dataframe from seaborn
+exercise = sns.load_dataset("exercise")
+
+sea = sns.FacetGrid(exercise, col="time",
+                    height=4, aspect=.5)
+
+sea.map(sns.barplot, "diet", "pulse",
+        order=["no fat", "low fat"])
+
+ 
